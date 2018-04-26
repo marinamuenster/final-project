59af164f8eacec7e838d4b4bd606a5c100fe6d6c
Quinn Harrison <m182472@usna.edu>
2018-04-26 11:42:24 -0400

    server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import random
 import json
 import requests
+import serial
 from flask import (Flask,
                    request,
                    url_for,
@@ -14,20 +15,20 @@ def home():
 
 @app.route("/data.json")
 def data():
-    # TODO read temperature and humidity from Arduino
+    # read temperature and humidity from Arduino
     s = serial.Serial("/dev/ttyACM0")  
     l = s.readline() 
-    #check to see if l is x's and print errors (print ???)
-    if(l == "X"):
-	indoor_temp == "???"
-	indoor_humidity == "???"
+    # check to see if l is x's and print errors 
+    x = l.rstrip().split(",")
+    if(x[0] == "X"):
+	indoor_temp = "???"
+	indoor_humidity = "???"
     else:
-        x = l.rstrip().split(",")
         indoor_temp = x[0]
         indoor_humidity = x[1]
 	
     # read temperature and humidity from openweathermap.org
-    r = request.get("http://api.openweathermap.org/data/2.5/"+
+    r = requests.get("http://api.openweathermap.org/data/2.5/"+
 		    "weather?id=4347242&units=imperial&"+
 		    "APPID=09f303a8a7a78820feda94e4d2ee47d6")
     data = r.json()
@@ -47,5 +48,14 @@ def cheep():
     message = request.form['message']
     print("got a cheep from [%s]: %s" % (name,message))
     # TODO: append [name: message] to a file of cheeps
+    
     # TODO: display the cheep on the kit LCD
+    
     return render_template('thankyou.html')
+
+
+
+
+
+
+
