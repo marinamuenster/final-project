79d49ac5cefe5008106307f0197eeb2a6873a38a
Quinn Harrison <m182472@usna.edu>
2018-04-29 20:04:52 -0400

    added gauges

--- static/script.js
+++ static/script.js
@@ -8,11 +8,15 @@ function getData(){
 	$("#indoor_humidity").text(weather.indoor_humidity)
 	$("#outdoor_temp").text(weather.outdoor_temp)
 	$("#outdoor_humidity").text(weather.outdoor_humidity)
-	//TODO: make other adjustments to UI based on these values
+	//make other adjustments to UI based on these values
 	
 	//or use a gauge from https://canvas-gauges.com/
 	//see: https://canvas-gauges.com/documentation/user-guide/configuration
-	$("#temp_gauge").attr("data-value",weather.indoor_temp)
+	
+	$("#indoor_temp_gauge").attr("data-value",weather.indoor_temp)
+	$("#outdoor_temp_gauge").attr("data-value",weather.outdoor_temp)
+	$("#indoor_humidity_gauge").attr("data-value",weather.outdoor_temp)
+	$("#outdoor_humidity_gauge").attr("data-value",weather.outdoor_temp)
     });
 }
 //runs as soon as document is ready
