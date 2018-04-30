caa1f7258d3654e2a665da38c9b57ad899b249ce
Marina Muenster <marina.muenster@gmail.com>
2018-04-29 22:55:06 -0400

    cheep handling

--- server.py
+++ server.py
@@ -48,8 +48,14 @@ def cheep():
     message = request.form['message']
     print("got a cheep from [%s]: %s" % (name,message))
     # TODO: append [name: message] to a file of cheeps
-    
+    newMsg = name + ": " + message + "/r/n"
+    file = open("/cheep.txt","a")
+    file.write(newMsg)
+    file.close()
     # TODO: display the cheep on the kit LCD
+    s = serial.Serial("/dev/ttyACM0")  
+    s.write(name)
+    s.write(message)
     
     return render_template('thankyou.html')
 
